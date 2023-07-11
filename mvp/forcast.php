b38a72e26b0f9431d79fafd42f7f54d961b80afe
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-07-11 10:37:45 +0200

    Add files via upload

---
+++ mvp/forcast.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Landing Page</title>
+    <link rel="stylesheet" href="st1.css">
+</head>
+<body>
+    <header>
+        <nav>
+           
+            <a href="#features">Forecast</a>
+            <a href="login.php">login</a>
+        </nav>
+        <div class="hero">
+            <h1>WeatherAPP</h1>
+            <p>Your go to waether website, with all the accuracy .</p>
+            <a href="user-registration.php" class="btn">Sign-Up</a> &nbsp; &nbsp;<a href="user-registtration.php" class="btn">login</a>
+        </div>
+    </header>
+    <main>
+        <section id="features">
+		<section id="image">
+    <div class="container">
+       
+    
+
+            <h2>Key Features</h2>
+            <div class="feature-card">
+                <img src="feature1.png" alt="">
+                <h3>accurate data</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div class="feature-card">
+                <img src="feature2.png" alt="">
+                <h3>5 day forcast</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div class="feature-card">
+                <img src="feature3.png" alt="">
+                <h3>Global weather</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+			
+      <br>    <br>    <br>    <br>    <br>    <br>    <br>
+          We bring you tomorrow's weather today .sighup and login and seewhat we have got for you 
+		  
+		  Stay ahead of the weather with our personalized features. Save your favorite places to quickly access their weather reports,<br>
+		  allowing you to plan your day accordingly. Whether it's monitoring the forecast for your hometown, dream destination, or upcoming business trip,<br>
+		  Weather App has got you covered.
+           
+			
+        </section> 
